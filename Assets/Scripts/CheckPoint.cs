9bc7d732d21d4f11f2301e176e15fbe23d9215da
mdengra9 <mireia.den.esp@gmail.com>
2024-01-12 13:19:15 +0100

    ygjt

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+   [SerializeField] private string userName;
+   [SerializeField] private int userScore;
+   [SerializeField] private Transform userPosition;
+   // Start is called before the first frame update
+   void Start()
+   {
+       LoadData();
+   }
+
+
+   public void SaveData()
+   {
+       PlayerPrefs.SetFloat("positionX", userPosition.x);
+       PlayerPrefs.SetFloat("positionY", userPosition.y);
+       PlayerPrefs.SetFloat("positionZ", userPosition.z);
+
+
+       LoadData();
+   }
+
+   void LoadData()
+   {
+    PlayerPrefs.GetFloat("positionX", 700.3867).ToString() + "x " +
+    PlayerPrefs.GetFloat("positionY", 77.83562).ToString() + "y " +
+    PlayerPrefs.GetFloat("positionZ", -128.094).ToString() + "z ";
+   }
+}
+
+
+
+
+
